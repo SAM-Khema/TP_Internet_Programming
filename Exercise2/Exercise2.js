f8f38d5b6e2d5d4107d33866a97c18318c1583b5
SAM-Khema <kemasam2001@gmail.com>
2023-03-28 09:01:17 +0700

    TP07

---
+++ Exercise2/Exercise2.js
@@ -0,0 +1,18 @@
+const express = require('express')
+                const fs = require('fs');
+                const app = express();
+                
+
+                app.get('/',function(req,res){
+                        fs.readFile('src/Exercise2.html','utf8',(err,data)=>{
+                                if(err){
+                                        console.log(err);
+                                        return;
+                                        
+                                }
+                                res.send(data);
+                        });
+                });
+                app.listen(3000,()=>{
+                        console.log("server is running on 3000");
+                })
